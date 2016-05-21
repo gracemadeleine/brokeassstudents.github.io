a61485e66299b5048c4acdbce3be97bb93c240bc
gracemadeleine <gracelinsdell@Graces-MacBook-Pro.local>
2016-05-21 10:28:23 +1000

    BAS
    BAS updates

---
+++ js/custom.js
@@ -0,0 +1,22 @@
+// waits for document ready - deprecated
+// $(document).ready(function(){
+// 		your javascript code goes here
+// });
+
+// this is the shorthand for document ready - used now
+// $(function(){
+// 		your javascript code goes here
+// });
+
+/*$(function(){
+	//js goes here
+	
+});
+
+	
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    document.getElementsByClassName("topnav")[0].classList.toggle("responsive");
+}
+
+
